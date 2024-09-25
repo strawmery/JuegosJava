8418c4fc33437b5f5100a09ed64eb02bc3b1de59
unknown <sealkevinboyabad@gmail.com>
2024-09-25 10:40:55 +0200

    avances

--- Conecta4.java
+++ Conecta4.java
@@ -1,17 +1,25 @@
 public class Conecta4 {
+
     public static void main(String[] args) {
+        Tablero();
+        char jugadorActual = JUGADOR1;
+    }
+
+    public static void Tablero() {
         
         int filas = 6;
         int columnas = 9;
         
         for (int i = 0; i < filas; i++) {
-            // Bucle para las columnas
             for (int j = 0; j < columnas; j++) {
-                // Imprimir símbolo sin salto de línea
                 System.out.print("* ");
             }
-            // Saltar a la siguiente línea después de cada fila
             System.out.println();
         }
     }
+    
+
+
 }
+
+
