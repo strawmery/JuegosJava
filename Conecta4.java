622e7e6df0c9f3a58321534f63c717e32c9a53ef
unknown <sealkevinboyabad@gmail.com>
2024-09-25 14:10:14 +0200

    se han añadido algunos métodos

--- Conecta4.java
+++ Conecta4.java
@@ -13,28 +13,35 @@ public class Conecta4 {
 
 
     //PASO 2: métodos
-
     public static void estadoInicial() {
         for (int f = 0, i < filas; i++)
             for (int c = 0, j < columnas; j++)
                 tablero[f][c] = asterisco;
     }
     
-    public static void Tablero() {
-        int filas = 6;
-        int columnas = 9;
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+    public static void estadoActual() {
+        for()
+
+    }
+
+    public static int verificar() {
+        int columna;
+        do {
+            System.out.print("Elige dónde insertar ficha (0-6): ");
         }
     }
 
-
+    public static boolean ponerFicha(int columna, char ficha) {
+        for(int i = filas ; i >= 0; i--) {
+            if(tablero[i][columna] == asterisco) {
+                tablero[i][columna] = ficha;
+                return true;
+            }
+        }
+        return false; // si no encuentro una posición para colocar la ficha retorno false
+    }
 
     //PASO 3: programa principal (método "main")
-
     public static void main(String[] args) {
         estadoInicial();
         estadoActual();
@@ -45,7 +52,8 @@ public class Conecta4 {
         System.out.println("Presiona una tecla " + (jugador == j1 ? "Jugador 1" : "Jugador 2"));
         int columna = leerColumna();
 
-        
+        if
+
     }
 
 }
