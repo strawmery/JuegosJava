94f0fb0a76bc9db863bc6a84b71add4206d30da5
unknown <sealkevinboyabad@gmail.com>
2024-09-26 06:25:49 -0700

    juego terminado

--- Conecta4.java
+++ Conecta4.java
@@ -5,31 +5,39 @@ public class Conecta4 {
 
     
     // PASO 1: declaración de variables
-    private static int filas = 6, columnas = 7;
-    private static char asterisco = '*', j1 = 'X', j2 = 'O';
+    private static int filas = 6;
+    private static int columnas = 7;
+    private static char asterisco = '*';
+    private static char j1 = 'X';
+    private static char j2 = 'O';
     private static char[][] tablero = new char [filas][columnas];
     private static Scanner sc = new Scanner(System.in);
+    private static boolean juegoTerminado = false;
 
 
 
     //PASO 2: métodos
     public static void estadoInicial() { // inicializa el juego ???
-        for (int f = 0, i < filas; i++)
-            for (int c = 0, j < columnas; j++)
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
                 tablero[f][c] = asterisco;
+                System.out.print(tablero[f][c] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("0 1 2 3 4 5 6");
     }
-    
     public static void estadoActual() { // muestra el tablero con las jugadas
-        for (int i = 0, i < filas; i++) {
-            for (int j = 0, j < columnas; j++) {
-                System.out.println(tablero[i][j] + " ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println("0 1 2 3 4 5 6"); 
     }
 
-    public static int elegirColumna(char jugador, Scanner sc) {
+    public static int elegirColumna(char jugador) {
         System.out.println("Elige una columna (0-6): ");
         return sc.nextInt();
     }
@@ -38,25 +46,23 @@ public class Conecta4 {
         return columna >= 0 && columna < columnas && tablero[0][columna] == asterisco;
     }
 
-    public static boolean ponerFicha(int columna, char ficha) {
-        for(int i = filas ; i >= 0; i--) {
+    public static int ponerFicha(int columna, char ficha) {
+        for(int i = filas -1; i >= 0; i--) {
             if(tablero[i][columna] == asterisco) {
                 tablero[i][columna] = ficha;
-                return true;
+                return i;
             }
-        }
-        return false; // si no encuentro una posición para colocar la ficha retorno false
+        } 
+        return -1; // si no encuentro una posición para colocar la ficha retorno false
     }
 
     private static boolean verificarGanador(int fila, int columna, char jugador) {
         int[][] direcciones = {{1, 0}, {0, 1}, {1, 1}, {1, -1}};
-
         for (int[] direccion : direcciones) {
             int contador = 0;
             for (int i = 0; i < 4; i++) {
                 int nuevaFila = fila + i * direccion[0];
                 int nuevaColumna = columna + i * direccion[1];
-                // Verificación directa sobre el tablero
                 if (0 <= nuevaFila && nuevaFila < filas && 0 <= nuevaColumna && nuevaColumna < columnas &&
                     tablero[nuevaFila][nuevaColumna] == jugador) {
                     contador++;
@@ -71,6 +77,17 @@ public class Conecta4 {
         return false;
     }
 
+    private static boolean tableroLleno() {
+        for (int i = 0; i < filas; i++) { 
+            for (int j = 0; j < columnas; j++) { 
+                if (tablero[i][j] == asterisco) {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
     private static void reiniciarJuego() {
         System.out.println("¿Jugar de nuevo? (s/n)");
         if (sc.next().equalsIgnoreCase("s")) {
@@ -83,38 +100,33 @@ public class Conecta4 {
     //PASO 3: programa principal (método "main")
     public static void main(String[] args) {
         estadoInicial();
-        estadoActual();
-        char jugadorJugando = j1;
-        boolean finalJuego = true; 
-
-    while (finalJuego != true) {
-        System.out.println("Presiona una tecla " + (jugador == j1 ? "Jugador 1" : "Jugador 2"));
-        int columna = verificar();
-
-        if(!ponerFicha(columna, jugadorJugando)) {
-            System.out.println("Intenta otra columna.");
-            continue;
-        }
-
-        estadoActual();
-
-
         
+        char[] jugadores = {j1, j2};
+        int jugadorActual = 0;
 
-
+        while (!juegoTerminado) {
+            estadoActual();
+            int columna = elegirColumna(jugadores[jugadorActual]);
+            if (verificarColumna(columna)) {
+                int fila = ponerFicha(columna, jugadores[jugadorActual]);
+                if (fila == -1) {
+                    System.out.println("No se puede colocar la ficha en esta columna. Intente de nuevo.");
+                } else {
+                    if (verificarGanador(fila, columna, jugadores[jugadorActual]) || tableroLleno()) {
+                        System.out.println("¡El juego ha terminado! " +
+                                (verificarGanador(fila, columna, jugadores[jugadorActual]) ?
+                                        "Ha ganado " + jugadores[jugadorActual] :
+                                        "Es un empate"));
+                        juegoTerminado = true;
+                    } else {
+                        jugadorActual = (jugadorActual + 1) % jugadores.length;
+                    }
+                }
+            } else {
+                System.out.println("Columna inválida. Intente de nuevo.");
+            }
+        }
+        reiniciarJuego();
+        }
     }
 
-
-
-
-}
-
-
-
-
-
-
-
-
-
-}
